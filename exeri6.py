5807b1684ff75a771630b06a3f3743946dfb6d0d
Rafael Vinicius Garcia <rafael_garcia1993@yahoo.com.br>
2021-08-04 15:13:57 -0300

    Create exeri6.py

---
+++ exeri6.py
@@ -0,0 +1,9 @@
+#Exercício Python 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = int(input("Digite um número"))
+d= n * 2
+t = n * 3
+r = n ** (1/2)         #opção de calcular a raíz quadrada
+print("O dobro de {} vale {}".format(n, d))
+print("O triplo de {} vale {}".format(n, t))
+print("A raiz quadrada de {} vale {:.2f}".format(n, r))
